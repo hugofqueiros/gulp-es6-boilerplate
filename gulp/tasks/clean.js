9e9fa1e61a6a65a7cc89339f04e9b0d7ed3f5c3c
Hugo Queirós <hugofqueiros@gmail.com>
2016-06-16 10:07:18 +0200

    fix(npm): missing babel-register package

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -8,11 +8,3 @@ gulp.task('clean', (done) => {
     gutil.log('Deleted folder:\n', '\t\t\t' + gutil.colors.red(dest));
     rimraf(dest, done);
 });
-
-/*
-gulp.task('clean-build', function() {
-    del(['build']).then(function(paths) {
-        gutil.log('Deleted files/folders:\n', '\t\t\t' + gutil.colors.red(paths.join('\n')));
-    });
-});
-*/
